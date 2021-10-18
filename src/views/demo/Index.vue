12d1170607cb72c5aeb19b10b186c16357aca966
jwentao <380793594@qq.com>
2021-10-18 15:31:00 +0800

    init demo

---
+++ src/views/demo/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="index" />
+</template>
+<script>
+export default {
+  name: 'Index',
+  mixins: [],
+  props: {},
+  data: () => ({}),
+  watch: {},
+  methods: {}
+};
+</script>
+<style lang='scss' scoped>
+.index {
+}
+</style>
