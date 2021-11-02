605375a2919f3842e8774b56b721c79db2d6affe
jwentao <380793594@qq.com>
2021-11-02 20:45:03 +0800

    feat: todo 微应用

--- main-app/src/main.ts
+++ main-app/src/main.ts
@@ -1,4 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { registerMicroApps, start, loadMicroApp } from 'qiankun'
+
+const mircoApps = [
+    {
+        name: 'vue2-ele',
+        entry: '//localhost:8080',
+        activeRule: '/vue2-ele',
+        container: "#render-view"
+    }
+]
+registerMicroApps(mircoApps);
+start();
+loadMicroApp({
+    name: 'vue2-ele',
+    entry: '//localhost/:8080',
+    container: '#render-view',
+  });
 
 createApp(App).mount('#app')
