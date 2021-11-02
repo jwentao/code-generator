b0b0d10fbc2ca8ec493ee3b6ed6a2776e16f6593
jwentao <380793594@qq.com>
2021-11-02 17:44:11 +0800

    feat: init main app

---
+++ main-app/src/components/layout.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+defineProps<{ msg: string }>();
+
+const count = ref(0);
+</script>
+
+<template>
+  <div id="app2"></div>
+
+</template>
+
+<style scoped>
+</style>
