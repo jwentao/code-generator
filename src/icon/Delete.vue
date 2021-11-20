7dd35563802805f4b37b3b0efe6b71a5de256c83
jwentao <jin_wentao@qq.com>
2021-11-20 23:34:08 +0800

    fix: eslint

--- src/icon/Delete.vue
+++ src/icon/Delete.vue
@@ -1,5 +1,5 @@
 <template>
-  <svg t="1637398223889" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="6298" width="200" height="200"><path d="M768 341.344L682.656 256 512 426.656 341.344 256 256 341.344 426.656 512 256 682.656 341.344 768 512 597.344 682.656 768 768 682.656 597.344 512z" p-id="6299" fill="#de5b7a"></path></svg>
+  <svg t="1637398223889" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="6298" width="200" height="200"><path d="M768 341.344L682.656 256 512 426.656 341.344 256 256 341.344 426.656 512 256 682.656 341.344 768 512 597.344 682.656 768 768 682.656 597.344 512z" p-id="6299" fill="#de5b7a" /></svg>
 </template>
 <script>
 export default {
