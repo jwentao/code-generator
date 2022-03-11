eedc6691569e24a8824692cbcb8fd47f94d504d7
jwentao <jin_wentao@qq.com>
2022-03-12 00:54:27 +0800

    feat: add test main app

---
+++ server/main-app/src/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'antd/dist/antd.css';
+import { registerMicroApps, start } from 'qiankun';
+
+registerMicroApps([
+    {
+        name: 'vue app',
+        entry: { scripts: ['//localhost:8080/main.js'] },
+        container: '#testContainer',
+        activeRule: '/testContainer',
+    },
+]);
+
+start();
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
